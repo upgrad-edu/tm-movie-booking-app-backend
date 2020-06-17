30448000417aa8d32e77a7c4b09a7ff0d55efad2
satishmahajan <satish.mahajan1@gmail.com>
2020-06-17 11:30:05 +0530

    Added toString method movie and Theatre class

--- 1.movieTicketBooking - before/src/main/java/Main.java
+++ 1.movieTicketBooking - before/src/main/java/Main.java
@@ -1,8 +1,15 @@
+import com.upgrad.mtb.beans.Movie;
+import com.upgrad.mtb.beans.Theatre;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args){
         ApplicationContext context = new ClassPathXmlApplicationContext("mtbBeans.xml");
+        Movie movie = (Movie) context.getBean("movie");
+        System.out.println(movie);
+
+        Theatre theatre =(Theatre) context.getBean("theatre");
+        System.out.println(theatre);
     }
 }
